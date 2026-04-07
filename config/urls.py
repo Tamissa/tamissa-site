74204d0d5c793f2eb6696cc6fc316bae16c2e73e
tamissa <tam-issa@gmail.com>
2026-04-07 18:13:10 +0200

    Serve media in production

--- config/urls.py
+++ config/urls.py
@@ -12,6 +12,4 @@ urlpatterns = [
     # and remove 'shop' from INSTALLED_APPS in settings.py
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
