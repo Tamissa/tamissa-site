f0cf412e9e11aff4187589b4c35fc7dc0c9d33e4
tamissa <tam-issa@gmail.com>
2026-04-07 20:18:24 +0200

    Fix media serving on Render

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
-    path('admin/',  admin.site.urls),
-    path('',        include('gallery.urls', namespace='gallery')),
+    path('admin/', admin.site.urls),
+    path('', include('gallery.urls', namespace='gallery')),
 
-    # ── shop app has been removed from routing ──
-    # If you want to fully remove it later, delete the shop/ directory
-    # and remove 'shop' from INSTALLED_APPS in settings.py
+    # Serve media files in production
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
